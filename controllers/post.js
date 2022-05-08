50c111cd758ca24e53c90896e829b4de38fe8dcc
konstante <konstantechang@gmail.com>
2022-05-08 23:10:19 +0800

    modified post.js comment

--- controllers/post.js
+++ controllers/post.js
@@ -74,6 +74,22 @@ const postController = {
     createPost: async (req, res, next) => {
         /* 
             #swagger.tags = ['Posts']
+            #swagger.description = '貼文API'
+            #swagger.parameters['body'] = {
+                in: 'body',
+                type: 'object',
+                description: '資料格式',
+                schema: {
+                    "user": "62713a3072d92f8a2b5ffaa0",
+                    "tags": "ring and soul",
+                    "type": "3D",
+                    "content": "Elden Ris is good.",
+                    "image": "https://fakeface.rest/thumb/view/20?minimum_age=25",
+                    "likes": 7777,
+                    "comments": 77777,
+}
+                }
+            }
         */
         
             const data = req.body;
